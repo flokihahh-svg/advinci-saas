aa9e4bca4e9630f09fd32af0c46f06eb254e046c
flokihahh-svg <flokihahh@gmail.com>
2026-05-26 01:15:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-import os
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
-def read_root():
-    # هذا الكود سيقرأ ملف index.html من مجلد templates ويظهره
-    file_path = "templates/index.html"
-    if os.path.exists(file_path):
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
-    else:
-        return "<h1>ملف index.html غير موجود، تأكد من رفعه داخل مجلد templates</h1>"
+# إعداد مكان ملفات التصميم
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
