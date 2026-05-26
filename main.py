6b07b0da11e9f97fdb71b7ac3a17f70b45c1e3e4
flokihahh-svg <flokihahh@gmail.com>
2026-05-26 06:52:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-# المجلد هنا اسمه "templates" (حروف صغيرة)
-templates = Jinja2Templates(directory="templates")
+# المسار المطلق لضمان عمل المجلدات
+BASE_DIR = os.path.dirname(os.path.abspath(file))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/")
 def read_root(request: Request):
-    # الملف هنا اسمه "index.html" (حروف صغيرة)
     return templates.TemplateResponse("index.html", {"request": request})
