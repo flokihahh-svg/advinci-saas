47001fb2ba1f399f9186b036a60705c6d3475ccd
flokihahh-svg <flokihahh@gmail.com>
2026-05-26 06:28:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# تعريف مكان ملفات التصميم
+# هذا السطر يبحث عن مجلد اسمه templates
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root(request: Request):
+    # هذا السطر يفتح index.html الموجود داخل المجلد
     return templates.TemplateResponse("index.html", {"request": request})
